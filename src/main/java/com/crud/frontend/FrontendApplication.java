7a9f3116beb664ed68ea8620815dee342c5aed69
SantyJustyna <justyna_santy@hotmail.com>
2024-09-07 13:11:43 +0200

    First commit

---
+++ src/main/java/com/crud/frontend/FrontendApplication.java
@@ -0,0 +1,13 @@
+package com.crud.frontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrontendApplication.class, args);
+	}
+
+}
